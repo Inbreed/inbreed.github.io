eabc31f37e8bb51bffc948950b49f4712cbff9af
Mattias Hagberg <mattias.hagberg@freespee.com>
2014-10-20 17:18:29 +0200

    SCSS

--- js/main.js
+++ js/main.js
@@ -1,28 +1,13 @@
-var images = [
-  'img/20140829_21_00234.jpg',
-  'img/20140829_21_00239.jpg',
-  'img/20140829_22_00293.jpg',
-  'img/20140829_22_00320-2.jpg',
-  'img/20140829_22_00329.jpg',
-  'img/20140829_22_00357-2.jpg',
-  'img/20140831_00_00582-2.jpg',
-  'img/20140831_01_00649.jpg',
-  'img/20140831_01_00711.jpg',
-  'img/20140831_01_00802.jpg',
-  'img/20140831_01_00841-2.jpg'
-];
-var bgIndex = 0, oldIndex;
-$(document).ready(function(){
-  index = Math.floor((Math.random()*images.length));
-  $.backstretch(images[index], {duration: 15000, fade: 3750});
-  setInterval(function() {
-    oldIndex = index;
-    while (oldIndex == index) {
-      index = Math.floor((Math.random()*images.length));
-    }
-    $.backstretch(images[index]);
-  }, 25000);
-  $(images).each(function() {
-    $('<img/>')[0].src = this;
-  });
-});
+var $d = $(document),
+    $w = $(window),
+    $h = $("header"),
+    $n = $("nav"),
+    $nd = $("nav div"),
+    $b = $("header div");
+
+$d.scroll(function(){
+  var s = $d.scrollTop(),
+      wh = $w.height();
+  $b.css("transform","translate3d(0,"+s/3+"px,0)");
+  (s>$h.height()-$n.height()) ? $n.addClass("fixed") : $n.removeClass("fixed");
+})
