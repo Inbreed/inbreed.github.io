940a6ad72dcaba2563c0e8286e38d5f933a7ca8e
Mattias Hagberg <mattias.hagberg@freespee.com>
2014-10-21 16:53:38 +0200

    Navigation

--- js/main.js
+++ js/main.js
@@ -11,3 +11,23 @@ $d.scroll(function(){
   $b.css("transform","translate3d(0,"+s/3+"px,0)");
   (s>$h.height()-$n.height()) ? $n.addClass("fixed") : $n.removeClass("fixed");
 })
+
+var navicon = document.querySelectorAll('.navicon');
+
+[].forEach.call(navicon, function(navicon){
+  var open = false;
+
+  navicon.onclick = function(event){2
+    event.preventDefault();
+
+    if(!open) {
+      this.classList.add('is--closed');
+      $('nav').addClass('open');
+      open = true;
+    } else {
+      this.classList.remove('is--closed');
+      $('nav').removeClass('open');
+      open = false;
+    }
+  }
+});
