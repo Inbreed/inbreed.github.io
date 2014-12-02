17dbda1203131b6b0616bf623ca8eef4833f7a69
Mattias Hagberg <mattias.hagberg@freespee.com>
2014-12-02 12:36:35 +0100

    Fix pageload infinite loop

--- js/main.js
+++ js/main.js
@@ -4,22 +4,17 @@ var $d = $(document),
     $n = $("nav"),
     $nd = $("nav div"),
     $b = $("header div");
-
 $d.scroll(function(){
   var s = $d.scrollTop(),
       wh = $w.height();
   $b.css("transform","translate3d(0,"+s/3+"px,0)");
   (s>$h.height()-$n.height()) ? $n.addClass("fixed") : $n.removeClass("fixed");
 })
-
 var navicon = document.querySelectorAll('.navicon');
-
 [].forEach.call(navicon, function(navicon){
   var open = false;
-
   navicon.onclick = function(event){2
     event.preventDefault();
-
     if(!open) {
       this.classList.add('is--closed');
       $('nav').addClass('open');
@@ -31,15 +26,3 @@ var navicon = document.querySelectorAll('.navicon');
     }
   }
 });
-/*
-$(function(){
-  $.getJSON('http://raw.githubusercontent.com/Inbreed/inbreed-android/master/www/test.json',function(data){
-    console.log('success');
-    $.each(data.bands,function(i,band){
-      $('.modals').append('<div>"'+band.name+' '+band.img+' '+band.desc+' '+band.time+'</div>');
-    });
-  }).error(function(){
-    console.log('error');
-  });
-});
-*/
